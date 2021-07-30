58d203204c9e5a4aea63ad4faa486c38dab7352d
Kumaresan Muthaliar <k.muthaliar@wolterskluwer.com>
2021-07-30 13:09:32 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnInit,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-}
+export class AppComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  constructor() {
+    console.log(`new - data is }`);
+  }
 
+  ngOnChanges() {
+    console.log(`ngOnChanges - data is }`);
+  }
+
+  ngOnInit() {
+    console.log(`ngOnInit  - data is }`);
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
